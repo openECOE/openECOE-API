c9cf7223053c9739879f2fb83e563b9a55deffe8
Cristina Puentes <cristina.puentes@goumh.umh.es>
2018-02-02 13:03:22 +0100

    Estructura inicial Organización

--- Organizacion.py
+++ Organizacion.py
@@ -11,7 +11,29 @@ class Organizacion(db.Model):
         self.usuarios = usuarios
         self.ecoes = ecoes
 
-
     def get_organizacion(self, id):
         organizacion = Organizacion.query.filter_by(id_organizacion=id).first()
         return organizacion
+
+    def post_organizacion(self):
+        organizacion = Organizacion(nombre=self.nombre)
+        db.session.add(organizacion)
+
+        db.session.commit()
+        return organizacion
+
+    #Editamos el nombre de la Organiación.
+    def put_organizacion(self, nombre):
+        organizacion = Organizacion.query.filter_by(id_organizacion=self.id_organizacion).first()
+        organizacion.nombre = nombre
+        db.session.commit()
+
+        return organizacion
+
+    def delete_organizacion(self):
+        organizacion = Organizacion.query.filter_by(id_organizacion=self.id_organizacion).first()
+
+        db.session.delete(organizacion)
+        db.session.commit()
+
+        return "OK"
