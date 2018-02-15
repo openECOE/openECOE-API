5b4e0386fa466f90828bb97ec23da1a829ed5da8
Ángel Segura <angel.segura@goumh.umh.es>
2018-02-15 12:51:56 +0100

    Creación de la API Pregunta

--- Grupo.py
+++ Grupo.py
@@ -1,13 +1,18 @@
 from db import db
+from Pregunta import Pregunta
 
-
+from db import app
+import numpy as np
+from flask import jsonify, request
+import json
+from werkzeug.exceptions import abort, Response
 
 class Grupo(db.Model):
     id_grupo = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(255))
     id_estacion = db.Column(db.Integer, db.ForeignKey('estacion.id_estacion'))
+    preguntas = db.relationship('Pregunta', backref='preguntas', lazy='dynamic')
 
-    preguntas = db.Column(db.Integer)
 
     def __init__(self, nombre='', preguntas='', id_estacion=0):
         self.nombre = nombre
@@ -25,7 +30,7 @@ class Grupo(db.Model):
         grupo = grupos[numGrupos-1]
 
         return grupo
-        return grupo
+
 
     def post_grupo(self):
         db.session.add(self)
@@ -39,3 +44,102 @@ class Grupo(db.Model):
     def delete_grupo(self):
         db.session.delete(self)
         db.session.commit()
+
+    def existe_grupo_pregunta(self, id_pregunta):
+        for pregunta in self.preguntas:
+            if(pregunta.id_pregunta==id_pregunta):
+                return True
+        return False
+
+@app.route('/api/v1.0/grupos/<int:grupo_id>/pregunta/', methods=['GET'])
+def obtenPreguntas(grupo_id):
+    grupo = Grupo().get_grupo(grupo_id)
+
+    if (grupo):
+        preguntas = []
+        for pregunta in grupo.preguntas:
+            preguntas.append({
+                "id_pregunta" : pregunta.id_pregunta,
+                "ref" : pregunta.ref,
+                "tipo_pregunta" : pregunta.tipo_pregunta
+            })
+
+        return json.dumps(preguntas, indent=1, ensure_ascii=False).encode('utf8')
+    else:
+        abort(404)
+
+
+@app.route('/api/v1.0/grupos/<int:grupo_id>/pregunta/<int:pregunta_id>/', methods=['GET'])
+def obtenPregunta(grupo_id, pregunta_id):
+    grupo = Grupo().get_grupo(grupo_id)
+
+    if (grupo):
+        if (grupo.existe_grupo_pregunta(pregunta_id)):
+            pregunta = Pregunta().get_pregunta(pregunta_id)
+            return jsonify({"id_pregunta" : pregunta.id_pregunta, "ref" : pregunta.ref, "tipo_pregunta" : pregunta.tipo_pregunta})
+        else:
+            abort(404)
+
+    else:
+        abort(404)
+
+
+@app.route('/api/v1.0/grupos/<int:grupo_id>/pregunta/', methods=['POST'])
+def insertaPregunta(grupo_id):
+    grupo = Grupo().get_grupo(grupo_id)
+
+    if (grupo):
+        value = request.json
+        ref = value["ref"]
+        tipo_pregunta = value["tipo_pregunta"]
+
+        preguntaIn = Pregunta(ref, tipo_pregunta, grupo_id)
+        preguntaIn.post_pregunta()
+
+        pregunta = Pregunta().get_ult_pregunta()
+
+        return jsonify({"id_pregunta": pregunta.id_pregunta, "ref": pregunta.ref, "tipo_pregunta": pregunta.tipo_pregunta})
+
+    else:
+        abort(404)
+
+
+@app.route('/api/v1.0/grupos/<int:grupo_id>/pregunta/<int:pregunta_id>/', methods=['PUT'])
+def modificaPregunta(grupo_id, pregunta_id):
+    grupo = Grupo().get_grupo(grupo_id)
+
+    if (grupo):
+        if (grupo.existe_grupo_pregunta(pregunta_id)):
+            value = request.json
+            ref = value["ref"]
+            tipo_pregunta = value["tipo_pregunta"]
+            id_grupo = value["id_grupo"]
+
+            pregunta = Pregunta().get_pregunta(pregunta_id)
+            pregunta.put_pregunta(ref, tipo_pregunta, id_grupo)
+
+            return jsonify({"id_pregunta" : pregunta.id_pregunta, "ref" : pregunta.ref, "tipo_pregunta" : pregunta.tipo_pregunta})
+        else:
+            abort(404)
+
+    else:
+        abort(404)
+
+
+@app.route('/api/v1.0/grupos/<int:grupo_id>/pregunta/<int:pregunta_id>/', methods=['DELETE'])
+def eliminaPregunta(grupo_id, pregunta_id):
+    grupo = Grupo().get_grupo(grupo_id)
+
+    if (grupo):
+        if (grupo.existe_grupo_pregunta(pregunta_id)):
+            pregunta = Pregunta().get_pregunta(pregunta_id)
+            pregunta.delete_pregunta()
+
+            return jsonify({"id_pregunta": pregunta.id_pregunta, "ref": pregunta.ref, "tipo_pregunta": pregunta.tipo_pregunta})
+        else:
+            abort(404)
+
+    else:
+        abort(404)
+
+
--- Pregunta.py
+++ Pregunta.py
@@ -2,39 +2,46 @@ from db import db
 
 class Pregunta(db.Model):
     id_pregunta = db.Column(db.Integer, primary_key=True)
-    ref = db.Column(db.String(255))
-    tipo_opcion = db.Column(db.Integer)
+    ref = db.Column(db.String(500))
+    tipo_pregunta = db.Column(db.String(255))
+    id_grupo = db.Column(db.Integer, db.ForeignKey('grupo.id_grupo'))
 
-    # TODO hacer que area y opcion sea relationship
-    area_pregunta = db.Column(db.Integer)
-    opciones = db.Column(db.Integer)
+    #area_pregunta = db.Column(db.Integer)
+    #opciones = db.Column(db.Integer)
 
-    def __init__(self, referencia, tipo_opcion, area_pregunta, opciones):
-        self.referencia = referencia
-        self.tipo_opcion = tipo_opcion
-        self.area_pregunta = area_pregunta
-        self.opciones = opciones
+    def __init__(self, referencia='', tipo_pregunta='', id_grupo=0):
+        self.ref = referencia
+        self.tipo_pregunta = tipo_pregunta
+        self.id_grupo = id_grupo
 
     def get_pregunta(self, id):
         pregunta = Pregunta.query.filter_by(id_pregunta=id).first()
         return pregunta
+
+    def get_ult_pregunta(self):
+        preguntas = Pregunta.query.all()
+
+        numPreguntas = len(preguntas)
+        pregunta = preguntas[numPreguntas - 1]
+
+        return pregunta
+
 
     def post_pregunta(self):
         db.session.add(self)
         db.session.commit()
 
     #Edita la ref de preguntas
-    def put_preguntaRef(self, ref):
+    def put_pregunta(self, ref, tipo_pregunta, id_grupo):
         self.ref = ref
-        db.session.commit()
-
-    # Edita el tipo de opción de preguntas
-    def put_preguntaTipoOpcion(self, tipo_opcion):
-        self.tipo_opcion = tipo_opcion
+        self.tipo_pregunta = tipo_pregunta
+        self.id_grupo = id_grupo
+        
         db.session.commit()
 
     def delete_pregunta(self):
         db.session.delete(self)
         db.session.commit()
 
-    #TODO faltan los métodos relacionados con Area y Opción
+
+#TODO Area
