0258d71b6728fc175d327f7adafc1b6e10ac4e11
Cristina Puentes <cristina.puentes@goumh.umh.es>
2018-02-16 15:11:42 +0100

    Métodos Día

--- Dia.py
+++ Dia.py
@@ -1,8 +1,18 @@
 from db import db
+from db import app
 
+import numpy as np
+import json
+
+from werkzeug.exceptions import abort, Response
+from flask import jsonify, request
+
+from ECOE import ECOE
+from Turno import Turno
+from Rueda import Rueda
 
 class Dia(db.Model):
-    cod_dia = db.Column(db.String(255), primary_key=True)
+    id_dia = db.Column(db.Integer, primary_key=True)
     fecha = db.Column(db.Integer)
 
     # TODO hacer que turnos sea relationship
@@ -13,8 +23,16 @@ class Dia(db.Model):
         self.fecha = fecha
         self.turnos = turnos
 
-    def get_dia(self, cod):
-        dia = Dia.query.filter_by(cod_dia=cod).first()
+    def get_dia(self, id):
+        dia = Dia.query.filter_by(id_dia=id).first()
+        return dia
+
+    def get_ult_dia(self):
+        dias = Dia.query.all()
+
+        numdias = len(dias)
+        dia = dias[numdias-1]
+
         return dia
 
     def post_dia(self):
@@ -28,4 +46,73 @@ class Dia(db.Model):
     def delete_dia(self):
         db.session.delete(self)
         db.session.commit()
+
+
+#RUTAS DE DIA
+@app.route('/api/v1.0/ECOE/<int:ecoe_id>/dias/<int:dia_id>', methods=['GET'])
+def muestraDia(dia_id):
+    dia = Dia().get_dia(dia_id)
+
+    if(dia):
+        return jsonify({"id_dia": dia.id_dia, "fecha": dia.fecha})
+
+    else:
+        abort(404)
+
+@app.route('/api/v1.0/ECOE/<int:ecoe_id>/dias/<int:dia_id>/turnos', methods=['GET'])
+def obtenTurnos(dia_id):
+    dia = Dia().get_dia(dia_id)
+
+    if(dia):
+        turnos =[]
+        for turno in dia.turnos:
+            turnos.append({
+                "id_turno": turno.id_turno,
+                "hora inicio": turno.hora_inicio,
+            })
+
+            return json.dump(turnos, indent=1, ensure_ascii=False).encode('utf8')
+        else:
+            abort(404)
+
+@app.route('/api/v1.0/ECOE/<int:ecoe_id>/dias', methods=['POST'])
+def insertaDia(ecoe_id):
+    ecoe = ECOE().get_ECOE(ecoe_id)
+
+    if(ecoe):
+        value = request.json
+        fecha = value["fecha"]
+
+        diaIn = Dia(fecha=fecha, id_ecoe=ecoe_id)
+        diaIn.post_dia()
+
+        dia = Dia().get_ult_dia()
+
+        return jsonify({"id_dia": dia.id_dia, "fecha": dia.fecha})
+    else:
+        abort(404)
+
+@app.route('/api/v1.0/ECOE/<int:ecoe_id>/dias/<int:dia_id>', methods=['PUT'])
+def modificaDia(dia_id):
+    dia = Dia().get_dia(dia_id)
+
+    if(dia):
+        value = request.json
+        fecha = value["fecha"]
+
+        dia.put_dia(fecha)
+
+        return jsonify({"id_dia": dia.id_dia, "fecha": dia.fecha})
+    else:
+        abort(404)
+
+@app.route('/api/v1.0/ECOE/<int:ecoe_id>/dias/<int:dia_id>', methods=['DELETE'])
+def eliminaDia(dia_id):
+    dia = Dia().get_dia(dia_id)
+
+    if(dia):
+        dia.delete_dia()
+        return jsonify({"id_dia": dia.id_dia, "fecha": dia.fecha})
+    else:
+        abort(404)
 
