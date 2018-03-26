130c6e318a265593805ee2b1b8cbed78dad10351
Segura <angel.segura@goumh.umh.es>
2018-03-26 23:56:42 +0200

    API: shift, round and round-student was created.

--- model/Shift.py
+++ model/Shift.py
@@ -7,7 +7,7 @@ class Shift(db.Model):
     __tablename__="shi"
 
     id_shift = db.Column(db.Integer, primary_key=True)
-    start_time = db.Column(db.TIMESTAMP)
+    start_time = db.Column(db.DATETIME)
     id_day = db.Column(db.Integer, db.ForeignKey(Day.id_day), nullable=False)
     day = db.relationship(Day, backref=backref('shifts', lazy='dynamic'))
 
