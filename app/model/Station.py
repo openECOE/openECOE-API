f5c31ab04ff1487928aa62448ebf0470ff196b94
Alberto Moreno <amoreno@goumh.umh.es>
2018-05-03 21:05:20 +0200

    Shift, Wheel refactory to create Planner

--- app/model/Station.py
+++ app/model/Station.py
@@ -5,8 +5,12 @@ class Station(db.Model):
     __tablename__ = 'station'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), unique=True)
+    name = db.Column(db.String(255))
     id_ecoe = db.Column(db.Integer, db.ForeignKey('ecoe.id'), nullable=False)
 
     schedules = db.relationship('Schedule', backref='station')
     qblocks = db.relationship('QBlock', backref='station')
+
+    __table_args__ = (
+        db.UniqueConstraint(name, id_ecoe, name='station_ecoe_uk'),
+    )
