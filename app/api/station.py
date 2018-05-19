0c11ba327673b9dc02ac8600e643424b2fb1caf0
Alberto Moreno <amoreno@goumh.umh.es>
2018-05-19 23:45:15 +0200

    Added order column to Station model

--- app/api/station.py
+++ app/api/station.py
@@ -1,4 +1,4 @@
-from flask_potion import ModelResource, fields
+from flask_potion import ModelResource, fields, signals
 from flask_potion.routes import Relation
 from app.model.Station import Station
 
@@ -14,4 +14,35 @@ class StationResource(ModelResource):
     class Schema:
         ecoe = fields.ToOne('ecoe')
 
+
+def order_station(item, op='add'):
+    order_correction = 0
+
+    stations_ecoe = len(item.ecoe.stations)
+    if not item.order or item.order > stations_ecoe or item.order < 1:
+        item.order = stations_ecoe
+    else:
+        stations_ecoe = Station.query \
+            .filter(Station.id_ecoe == item.ecoe.id).filter(Station.order >= item.order) \
+            .filter(Station.id != item.id).order_by(Station.order).all()
+
+        if op == 'add':
+            order_correction = 1
+
+        for order, station_ecoe in enumerate(stations_ecoe):
+            station_ecoe.order = order + item.order + order_correction
+
+@signals.before_update.connect_via(StationResource)
+def before_update_station(sender, item, changes):
+    if 'order' in changes.keys():
+        item.order = changes['order']
+        order_station(item)
+
+@signals.before_create.connect_via(StationResource)
+def before_create_station(sender, item):
+    order_station(item)
+
+@signals.before_delete.connect_via(StationResource)
+def before_delete_station(sender, item):
+    order_station(item, 'del')
 
--- app/model/Station.py
+++ app/model/Station.py
@@ -1,5 +1,5 @@
 from app import db
-
+from sqlalchemy.event import listen
 
 class Station(db.Model):
     __tablename__ = 'station'
@@ -7,6 +7,7 @@ class Station(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
     id_ecoe = db.Column(db.Integer, db.ForeignKey('ecoe.id'), nullable=False)
+    order = db.Column(db.Integer)
 
     schedules = db.relationship('Schedule', backref='station')
     qblocks = db.relationship('QBlock', backref='station')
