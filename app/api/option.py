400d2cefcf378561c9e67fc533b1e80b65fe8f81
Alberto Moreno <amoreno@goumh.umh.es>
2019-11-07 10:37:26 +0100

    Added GPL3 License

--- app/api/option.py
+++ app/api/option.py
@@ -1,5 +1,22 @@
+#  Copyright (c) 2019 Miguel Hernandez University of Elche
+#  This file is part of openECOE-API.
+#
+#      openECOE-API is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#      (at your option) any later version.
+#
+#      openECOE-API is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with openECOE-API.  If not, see <https://www.gnu.org/licenses/>.
+
 from flask_potion import ModelResource, fields
 from app.model.Option import Option
+from app.model.User import RoleType
 
 
 class OptionResource(ModelResource):
@@ -7,6 +24,14 @@ class OptionResource(ModelResource):
     class Meta:
         name = 'options'
         model = Option
+
+        permissions = {
+            'read': 'manage',
+            'create': 'manage',
+            'update': 'manage',
+            'delete': 'manage',
+            'manage': ['manage:question', RoleType.ADMIN, RoleType.EVAL]
+        }
 
     class Schema:
         question = fields.ToOne('questions')
--- app/api/question.py
+++ app/api/question.py
@@ -1,3 +1,19 @@
+#  Copyright (c) 2019 Miguel Hernandez University of Elche
+#  This file is part of openECOE-API.
+#
+#      openECOE-API is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#      (at your option) any later version.
+#
+#      openECOE-API is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with openECOE-API.  If not, see <https://www.gnu.org/licenses/>.
+
 from flask_potion import fields
 from flask_potion.routes import Relation, ItemRoute
 from app.model.Question import Question, QType
@@ -17,7 +33,7 @@ class QuestionResource(PrincipalResource):
 
         permissions = {
             'read': 'read:area',
-            'create': 'manage',
+            'create': 'managCe',
             'update': 'manage',
             'delete': 'manage',
             'manage': 'manage:area'
