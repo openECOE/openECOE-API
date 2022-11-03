6c1e20c6dcf9e06baf176393f6daf5975ccc5c2c
Pasqual <pasqual.martinezb@umh.es>
2022-11-03 14:11:42 +0100

    Función y endpoint para devolver el porcentaje de acierto por Item de la ECOE

--- app/api/ecoe.py
+++ app/api/ecoe.py
@@ -31,8 +31,8 @@ from app.jobs import statistics as jobs_statistics
 from app.model.ECOE import ECOE, ChronoNotFound, ECOEstatus
 from app.model.User import PermissionType
 import os
-from flask import send_file, send_from_directory, current_app, request
-from app.statistics import generar_csv, resultados_evaluativo_ecoe, get_results_for_area
+from flask import send_file, current_app, request
+from app.statistics import generar_csv, resultados_evaluativo_ecoe, get_results_for_area, get_items_score
 from app.auth import auth
 class Location(int, Enum):
     ARCHIVE_ONLY = 1
@@ -322,6 +322,13 @@ class EcoeResource(OpenECOEResource):
         id_area = request.args['area']
         id_ecoe = str(ecoe.id)
         return get_results_for_area(id_area, id_ecoe)   
+
+    @ItemRoute.GET("/item-score", rel='items_score_by_ecoe')
+    def send_items_score(self, ecoe):
+        object_permissions = self.manager.get_permissions_for_item(ecoe)
+        if "manage" in object_permissions and object_permissions["manage"] is not True:
+            raise Forbidden
+        return get_items_score(id_ecoe=str(ecoe.id))
 
     @ItemRoute.GET("/configuration", rel="chronoSchema")
     def configuration(self, ecoe) -> fields.String():
--- app/statistics/__init__.py
+++ app/statistics/__init__.py
@@ -17,3 +17,4 @@
 from app.statistics.Resultados import resultados_evaluativo_ecoe
 from app.statistics.Csv import generar_csv
 from app.statistics.ResultsForArea import get_results_for_area
+from app.statistics.ItemScore import get_items_score
