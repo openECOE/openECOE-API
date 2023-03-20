56eae8e14acbcd9614a3ef954453ce5261ba5441
Alberto Moreno <amoreno@umh.es>
2023-03-20 21:07:55 +0100

    Gunicorn Eventlet

--- .docker/deploy/gunicorn.sh
+++ .docker/deploy/gunicorn.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /app/api && gunicorn --daemon --capture-output --enable-stdio-inheritance --workers=8 --bind=unix:/run/ecoe-api.sock openECOE-API:app
+cd /app/api && gunicorn -k eventlet --daemon --capture-output --enable-stdio-inheritance --workers=8 --bind=unix:/run/ecoe-api.sock openECOE-API:app
