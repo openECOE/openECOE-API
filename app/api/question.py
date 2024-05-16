4e0664d845d2646e91fd905d287139e710bf7e06
miguelaf0 <miapafe@gmail.com>
2024-05-16 12:39:45 +0200

    Implement reordering on edit and delete questions and block

--- app/api/question.py
+++ app/api/question.py
@@ -14,10 +14,11 @@
 #      You should have received a copy of the GNU General Public License
 #      along with openECOE-API.  If not, see <https://www.gnu.org/licenses/>.
 
-from flask_potion import fields
+from flask_potion import fields, signals
 from flask_potion.routes import Relation
 from app.model.Question import Block, Question
 from app.api._mainresource import OpenECOEResource
+from app.shared import order_items, calculate_order
 
 station_permissions = {
             'read': 'read:station',
@@ -54,3 +55,40 @@ class BlockResource(OpenECOEResource):
 
     class Schema:
         station = fields.ToOne('stations')
+
+def recalculate_question_order(id_station):
+    blocks = Block.query.filter(Block.id_station == id_station).order_by(Block.order).all()
+    unordered_questions = []
+    for block in blocks:
+        questions_by_block = [question for question in Question.query.filter(Question.id_block == block.id).order_by(Question.order).all()]
+        unordered_questions.extend()
+
+    calculate_order(unordered_questions)
+
+@signals.before_update.connect_via(BlockResource)
+def before_update_block(sender, item, changes):
+    if 'order' in changes.keys():
+        blocks = Block.query.filter(Block.id_station == item.id_station).order_by(Block.order).all()
+        order_items(item, blocks, changes['order'], 'add')
+        recalculate_question_order(item.id_station)
+
+@signals.before_delete.connect_via(BlockResource)
+def before_delete_block(sender, item):
+    Question.query.filter(Question.id_station == item.id).delete()
+    blocks = Block.query.filter(Block.id_station == item.id_station).order_by(Block.order).all()
+
+    if len(blocks) > 1:
+        order_items(item, blocks, item.order, 'del')
+        recalculate_question_order(item.id_station)
+
+@signals.before_update.connect_via(QuestionResource)
+def before_update_question(sender, item, changes):
+    if 'order' in changes.keys():
+        questions = Question.query.filter(Question.id_station == item.id_station).order_by(Question.order).all()
+        order_items(item, questions, changes['order'], 'add')
+
+@signals.before_delete.connect_via(QuestionResource)
+def before_delete_question(sender, item):
+    questions = Question.query.filter(Question.id_station == item.id_station).order_by(Question.order).all()
+    if len(questions) > 1:
+        order_items(item, questions, item.order, 'del')
