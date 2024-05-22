7fe835920eef4ab50d67cf90172a25d5be480b68
miguelaf0 <miapafe@gmail.com>
2024-05-22 10:49:19 +0200

    Fix ordering when new order is negative or bigger than the size of the list

--- app/shared/utils.py
+++ app/shared/utils.py
@@ -1,12 +1,16 @@
 def order_items(item, items, new_order, operation):
     # Posibles operaciones add y del (añadir y borrar)
     # Los items del array tienen que tener la propiedad order
-    if item.order > len(items) or item.order < 1:
-        item.order = len(items)
+    if new_order < 1:
+        item.order = 1
     else:
         item_idx = item.order - 1
         if operation == 'add':
-            items.insert(new_order - 1, items.pop(item_idx))
+            position = new_order - 1
+            if new_order > len(items):
+                position = len(items)
+                
+            items.insert(position, items.pop(item_idx))
         else:
             items.pop(item_idx)
         
