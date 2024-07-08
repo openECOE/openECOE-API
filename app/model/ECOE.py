4f9ee79f62c870f4678272880cb235183f78894e
miguelaf0 <miapafe@gmail.com>
2024-07-08 11:48:54 +0000

    Remove duplicate ecoe name constraint

--- app/model/ECOE.py
+++ app/model/ECOE.py
@@ -46,7 +46,7 @@ class ECOE(db.Model):
     __tablename__ = "ecoe"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), unique=True)
+    name = db.Column(db.String(255))
     id_organization = db.Column(
         db.Integer, db.ForeignKey("organization.id"), nullable=False
     )
